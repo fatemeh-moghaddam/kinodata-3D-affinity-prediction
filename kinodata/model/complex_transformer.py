7e92a1d3f851c4f385fc535876270293f3d2a01e
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-03-12 12:48:00 +0100

    saving hl from code directly

--- kinodata/model/complex_transformer.py
+++ kinodata/model/complex_transformer.py
@@ -164,6 +164,7 @@ class ComplexTransformer(RegressionModel):
         super().__init__(config)
         assert len(config["node_types"]) == 1
         assert config["node_types"][0] == NodeType.Complex
+        self.prob = prob
         self.act = resolve_act(act)
         self.d_cut = interaction_radius
         self.max_num_neighbors = max_num_neighbors
@@ -223,7 +224,7 @@ class ComplexTransformer(RegressionModel):
             self.atomic_num_embedding(node_store.z)
             + self.lin_atom_features(node_store.x)
         )
-        intermediate_node_reprs = {}
+        # intermediate_node_reprs = {}
         # intermediate_edge_reprs = {}
         # intermediate_edge_indecies = {}
 
@@ -237,13 +238,16 @@ class ComplexTransformer(RegressionModel):
             node_repr = norm(node_repr, node_store.batch)
             # write out embeddings
             if self.prob:
-                intermediate_node_reprs[f"layer_{l+1}"] = node_repr
-            # intermediate_edge_reprs[f"layer_{l+1}"] = edge_repr
-            # intermediate_edge_indecies[f"layer_{l+1}"] = edge_index
+                torch.save(node_repr, f"node_repr_{l}.pt")
+                torch.save(edge_repr, f"edge_repr_{l}.pt")
+                torch.save(edge_index, f"edge_index_{l}.pt")
+                # intermediate_node_reprs[f"layer_{l+1}"] = node_repr
+                # intermediate_edge_reprs[f"layer_{l+1}"] = edge_repr
+                # intermediate_edge_indecies[f"layer_{l+1}"] = edge_index
 
         graph_repr = self.aggr(node_repr, node_store.batch)
-        if self.prob:
-            return self.out(graph_repr), intermediate_node_reprs
+        # if self.prob:
+            # return self.out(graph_repr), intermediate_node_reprs, intermediate_edge_reprs, intermediate_edge_indecies
         return self.out(graph_repr)
 
 
