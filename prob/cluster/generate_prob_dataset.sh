24355b64519d046e7a32faf3790fe1ce7436377b
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-09-10 17:10:13 +0200

    transfer prob and kinodata

--- prob/cluster/generate_prob_dataset.sh
+++ prob/cluster/generate_prob_dataset.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-
-
 PROJECT_NAME="kinodata-3D-affinity-prediction"
 HOME_PROJ_DIR="${HOME}/${PROJECT_NAME}"
 cd "${HOME_PROJ_DIR}"
 
-echo "PWD: $(pwd)"
-export PYTHONPATH="$PWD:$PYTHONPATH"
 # Run from the job's working directory; assume required code is transferred with the job
 python3 prob/$1.py --split_type "$2" --rmsd_cutoff "$3" --gnn_model_type "$4"
