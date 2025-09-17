715167fe35ae606ab5e3cc00e589ce4bc5e41356
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-09-17 12:10:34 +0200

    install colorama

--- prob/cluster/generate_prob_dataset.sh
+++ prob/cluster/generate_prob_dataset.sh
@@ -4,5 +4,7 @@ HOME_PROJ_DIR="${HOME}/${PROJECT_NAME}"
 cd "${HOME_PROJ_DIR}"
 export PYTHONPATH="${HOME_PROJ_DIR}:${PYTHONPATH:-}"
 
+pip install -e .
+pip instal colorama
 # Run from the job's working directory; assume required code is transferred with the job
 python3 prob/$1.py --split_type "$2" --rmsd_cutoff "$3" --gnn_model_type "$4"
