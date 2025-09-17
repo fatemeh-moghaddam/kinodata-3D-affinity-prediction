86fd9a5c2c6dc9eb12819090b3e14688e801a67e
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-09-17 13:32:10 +0200

    stupid l!

--- prob/cluster/generate_prob_dataset.sh
+++ prob/cluster/generate_prob_dataset.sh
@@ -4,6 +4,6 @@ HOME_PROJ_DIR="${HOME}/${PROJECT_NAME}"
 cd "${HOME_PROJ_DIR}"
 export PYTHONPATH="${HOME_PROJ_DIR}:${PYTHONPATH:-}"
 
-pip instal colorama
+pip install colorama
 # Run from the job's working directory; assume required code is transferred with the job
 python3 prob/$1.py --split_type "$2" --rmsd_cutoff "$3" --gnn_model_type "$4"
