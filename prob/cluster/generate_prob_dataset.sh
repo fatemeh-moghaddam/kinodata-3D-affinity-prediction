f7607552d35588103417e0339ce167dc8b71903e
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-09-17 13:50:56 +0200

    argument name foxed, avoid pip>24

--- prob/cluster/generate_prob_dataset.sh
+++ prob/cluster/generate_prob_dataset.sh
@@ -4,6 +4,8 @@ HOME_PROJ_DIR="${HOME}/${PROJECT_NAME}"
 cd "${HOME_PROJ_DIR}"
 export PYTHONPATH="${HOME_PROJ_DIR}:${PYTHONPATH:-}"
 
+# Ensure pip stays below 24 to avoid non-standard specifier enforcement warnings
+python3 -m pip install --upgrade "pip<24"
 pip install colorama
 # Run from the job's working directory; assume required code is transferred with the job
-python3 prob/$1.py --split_type "$2" --rmsd_cutoff "$3" --gnn_model_type "$4"
+python3 prob/$1.py --split_type "$2" --filter_rmsd_max_value "$3" --gnn_model_type "$4"
