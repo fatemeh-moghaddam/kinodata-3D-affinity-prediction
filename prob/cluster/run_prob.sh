fc19c87536007dd58711c8a05c3f9621a847e62a
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-09-25 14:45:42 +0200

    run prob sh and sub

---
+++ prob/cluster/run_prob.sh
@@ -0,0 +1,13 @@
+
+PROJECT_NAME="kinodata-3D-affinity-prediction"
+HOME_PROJ_DIR="${HOME}/${PROJECT_NAME}"
+cd "${HOME_PROJ_DIR}"
+export PYTHONPATH="${HOME_PROJ_DIR}:${PYTHONPATH:-}"
+
+export WANDB_API_KEY=$(cat wandb_api_key)
+
+# Ensure pip stays below 24 to avoid non-standard specifier enforcement warnings
+python3 -m pip install --upgrade "pip<24"
+pip install --upgrade "wandb>=0.15,<1" colorama
+# Run from the job's working directory; assume required code is transferred with the job
+python3 prob/$1.py --split_type "$2" --filter_rmsd_max_value "$3" --gnn_model_type "$4"
