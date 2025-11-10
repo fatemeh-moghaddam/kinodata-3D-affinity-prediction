3dc8e3b4ec6a8b0d2f6e4e319cbf9fc9f21dca04
fatemeh-moghaddam <fatemeh.moghaddam9696@gmail.com>
2025-11-10 16:34:16 +0100

    just random split for now

--- prob/cluster/run_prob.sh
+++ prob/cluster/run_prob.sh
@@ -1,6 +1,7 @@
 
 PROJECT_NAME="kinodata-3D-affinity-prediction"
 HOME_PROJ_DIR="${HOME}/${PROJECT_NAME}"
+# CPU_COUNT="64"
 cd "${HOME_PROJ_DIR}"
 export PYTHONPATH="${HOME_PROJ_DIR}:${PYTHONPATH:-}"
 
@@ -9,5 +10,6 @@ export WANDB_API_KEY=$(cat wandb_api_key)
 # Ensure pip stays below 24 to avoid non-standard specifier enforcement warnings
 python3 -m pip install --upgrade "pip<24"
 pip install --upgrade "wandb>=0.15,<1" colorama
+
 # Run from the job's working directory; assume required code is transferred with the job
 python3 prob/$1.py --split_type "$2" --filter_rmsd_max_value "$3" --gnn_model_type "$4"
